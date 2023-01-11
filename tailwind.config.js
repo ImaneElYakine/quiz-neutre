d33659a037ce765d071372528344e664e0c27acc
Imane El Yakine <64931446+ImaneElYakine@users.noreply.github.com>
2023-01-11 15:47:23 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html', 'accueil.html', 'quiz.html'],
+    theme: {
+        extend: {
+            colors: {
+                'bleu' : '#0032FF',
+                'transparent' : 'rgba(0, 0, 0, 0.0)',
+                'vert' : '#82D200',
+                'rouge' : '#FF2144',
+                'jaune' : '#FFD700'
+            },
+            fontFamily: {
+                'mediametrie': ['Mediametrie', 'sans-serif'],
+            },
+            backgroundImage: {
+                'background': "url('img/background.svg')",
+            },
+            keyframes: {
+                slide: {
+                    '0%': { transform: 'translateX(-10px)', opacity: '0'},
+                    '100%': { transform: 'translateX(0px)' , opacity: '1'}
+                },
+                appear: {
+                    '0%': { opacity: '0'},
+                    '100%': { opacity: '1'}
+                },
+            },
+            animation: {
+                'slide-left': 'slide 1.5s linear',
+                'appear': 'appear 1.5s linear',
+            },
+        },
+    },
+    plugins: [],
+}
